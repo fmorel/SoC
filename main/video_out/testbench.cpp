11911f82f077e5f794ff67c6a629e5eedeaf810e
Jon Harper <jon.harper87@gmail.com>
2010-06-08 18:32:28 +0200

    test file for video_out

--- main/video_out/testbench.cpp
+++ main/video_out/testbench.cpp
@@ -13,6 +13,39 @@ using namespace soclib::caba;
 using namespace sc_core;
 
 int _main(int argc, char* argv[]) {
+  
+  typedef soclib::caba::WbParams<32,32> wb_param;
+  VideoOut<wb_param> videoOut("Video_out");
+
+  sc_time     clk_periode(10, SC_NS); // clk period
+  sc_clock	signal_clk("signal_clk",clk_periode);
+  sc_time     video_clk_periode(40, SC_NS); // clk period
+  sc_clock	signal_video_clk("signal_video_clk",video_clk_periode);
+  sc_signal<bool> signal_resetn("signal_resetn");
+
+  sc_signal<bool>             signal_line_valid;
+  sc_signal<bool>             signal_frame_valid;
+  sc_signal<unsigned char>    signal_pixel_out;
+
+  sc_signal<bool>            signal_interrupt;
+
+  soclib::caba::WbSignal<wb_param> signal_wb_video_out_slave("signal_wb_video_out_slave");
+  soclib::caba::WbSignal<wb_param> signal_wb_video_out_master("signal_wb_video_out_master");
+
+  videoOut.p_clk(signal_clk);
+  videoOut.p_video_clk(signal_video_clk);
+  videoOut.p_resetn(signal_resetn);
+
+  videoOut.p_line_valid(signal_line_valid);
+  videoOut.p_frame_valid(signal_frame_valid);
+  videoOut.p_pixel_out(signal_pixel_out);
+
+  videoOut.p_interrupt(signal_interrupt);
+  videoOut.p_wb_master(signal_wb_video_out_master);
+  videoOut.p_wb_slave(signal_wb_video_out_slave);
+
+  //TODO
+  //Est-ce qu'on veut tester ce module indépendant du wishbone ?
 
   return EXIT_FAILURE;
 }
