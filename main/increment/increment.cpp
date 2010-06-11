278fa6378ea0b833e304a18338de7fd55aac09df
Alexandre Rames <rames@rossak.enst.fr>
2010-06-11 15:37:17 +0200

    Basis for new increment module.

--- main/increment/increment.cpp
+++ main/increment/increment.cpp
@@ -2,69 +2,187 @@
 #include "increment_hard.h"
 #include <iostream>
 
+using namespace std;
+
 namespace soclib { namespace caba {
-	// Constructor
-	template <typename wb_param> \
-		Increment<wb_param>::Increment(sc_core::sc_module_name insname)
-		: sc_core::sc_module(insname)
-     ,incrementHard("increment hard")
-     ,interpolation("interpolation")
-     ,buffer("buffer")
-		{
-			std::cout << "WishBone increment"  << name() 
-				<< " created sucsesfully" << std::endl;
+    // Constructor
+    template <typename wb_param> \
+        Increment<wb_param>::Increment(sc_core::sc_module_name insname)
+        : sc_core::sc_module(insname),
+        incHardX("incHardX"),
+        incHardY("incHardY")
+//          ,interpolation("interpolation")
+//          ,buffer("buffer")
+    {
+        std::cout << "WishBone increment"  << name() 
+            << " created sucsesfully" << std::endl;
 
-			SC_METHOD(incrementTransition);
-			dont_initialize();
-			sensitive<<p_clk.pos();
-			SC_METHOD(incrementMoore);
-			dont_initialize();
-			sensitive<<p_clk.neg();
+        SC_METHOD(slaveTransition);
+        dont_initialize();
+        sensitive << p_clk.pos();
+        SC_METHOD(slaveMoore);
+        dont_initialize();
+        sensitive << p_clk.neg();
 
-      incrementHard.p_resetn(p_resetn);
-      incrementHard.p_clk(p_clk);
-      incrementHard.p_init_mux_switch(dummy_bool);
-      incrementHard.p_Q0(dummy_float);
-      incrementHard.p_Q1(dummy_float);
-      incrementHard.p_Q2(dummy_float);
-      incrementHard.p_Q3(dummy_float);
-      incrementHard.p_R0(dummy_float);
-      incrementHard.p_R1(dummy_float);
-      incrementHard.p_R2(dummy_float);
-      incrementHard.p_S0(dummy_float);
-      incrementHard.p_S1(dummy_float);
-      incrementHard.p_P0(dummy_float);
-      incrementHard.p_OUT(dummy_float1);
+        SC_METHOD(masterTransition);
+        dont_initialize();
+        sensitive << p_clk.pos();
+        SC_METHOD(masterMoore);
+        dont_initialize();
+        sensitive << p_clk.neg();
 
-      buffer.p_clk(p_clk);
-      buffer.p_resetn(p_resetn);
-      buffer.buffer_in(dummy_uint32);
-      buffer.buffer_write_enable(dummy_bool);
-      for(int i=0;i<4;i++)
-        buffer.buffer_out[i] (dummy_uchar[i]);
-      for (int i = 0;i<2;i++)
-        buffer.buffer_command_out[i] (dummy_uint32_t[i]);
-      
-      interpolation.p_clk(p_clk);
-      interpolation.p_resetn(p_resetn);
-      interpolation.x(dummy_float);
-      interpolation.y(dummy_float);
-      interpolation.out(dummy_out);
-      for(int i=0;i<4;i++)
-        interpolation.buffer_in[i] (dummy_uchar[i]);
-      for (int i = 0;i<2;i++)
-        interpolation.buffer_command[i] (dummy_uint32_t[i]);
-      
-		}
+        incHardX.p_resetn(p_resetn);
+        incHardX.p_clk(p_clk);
+        incHardX.p_init_mux_switch(dummy_bool);
+        incHardX.p_Q0(dummy_float);
+        incHardX.p_Q1(dummy_float);
+        incHardX.p_Q2(dummy_float);
+        incHardX.p_Q3(dummy_float);
+        incHardX.p_R0(dummy_float);
+        incHardX.p_R1(dummy_float);
+        incHardX.p_R2(dummy_float);
+        incHardX.p_S0(dummy_float);
+        incHardX.p_S1(dummy_float);
+        incHardX.p_P0(dummy_float);
+        incHardX.p_OUT(dummy_float1);
 
-	template <typename wb_param> \
-	void Increment<wb_param>::incrementTransition() {
-  }
+        incHardY.p_resetn(p_resetn);
+        incHardY.p_clk(p_clk);
+        incHardY.p_init_mux_switch(dummy_bool);
+        incHardY.p_Q0(dummy_float);
+        incHardY.p_Q1(dummy_float);
+        incHardY.p_Q2(dummy_float);
+        incHardY.p_Q3(dummy_float);
+        incHardY.p_R0(dummy_float);
+        incHardY.p_R1(dummy_float);
+        incHardY.p_R2(dummy_float);
+        incHardY.p_S0(dummy_float);
+        incHardY.p_S1(dummy_float);
+        incHardY.p_P0(dummy_float);
+        incHardY.p_OUT(dummy_float1);
 
-	template <typename wb_param> \
-	void Increment<wb_param>::incrementMoore() {
-	}
-		
+//        buffer.p_clk(p_clk);
+//        buffer.p_resetn(p_resetn);
+//        buffer.buffer_in(dummy_uint32);
+//        buffer.buffer_write_enable(dummy_bool);
+//        for(int i=0;i<4;i++)
+//            buffer.buffer_out[i] (dummy_uchar[i]);
+//        for (int i = 0;i<2;i++)
+//            buffer.buffer_command_out[i] (dummy_uint32_t[i]);
+//
+//        interpolation.p_clk(p_clk);
+//        interpolation.p_resetn(p_resetn);
+//        interpolation.x(dummy_float);
+//        interpolation.y(dummy_float);
+//        interpolation.out(dummy_out);
+//        for(int i=0;i<4;i++)
+//            interpolation.buffer_in[i] (dummy_uchar[i]);
+//        for (int i = 0;i<2;i++)
+//            interpolation.buffer_command[i] (dummy_uint32_t[i]);
+
+    }
+
+    template <typename wb_param> \
+        void Increment<wb_param>::slaveTransition() {
+            int32_t next_state;
+
+            // Reset handling.
+            if(!p_resetn) {
+                printf("INCREMENT: Reset.\n");
+                slave_state = WAIT_CONFIG;
+                coeff_address_start = 0;
+                coeff_address_current = 0;
+                return;
+            }
+
+            switch(slave_state) {
+
+                case IDLE:
+                    next_state = WAIT_CONFIG;
+                    break;
+
+                case WAIT_CONFIG:
+                    if (p_wb_slave.STB_I && p_wb_slave.CYC_I) {
+                        if(p_wb_slave.WE_I)
+                            next_state = LOAD_COEFF;
+                        else
+                            printf("INCREMENT: Something tried to read us.");
+                    } else {
+                        next_state = WAIT_CONFIG;
+                    }
+                    break;
+
+                case ACK_AND_LOAD_COEFF:
+                    next_state = LOAD_COEFF;
+                    break;
+
+                case LOAD_COEFF:
+                    break;
+
+                default:
+                    next_state = IDLE;
+            }
+
+            slave_state = next_state;
+        }
+
+
+    template <typename wb_param> \
+        void Increment<wb_param>::masterTransition() {
+            int32_t next_state;
+
+            switch(master_state) {
+
+                case IDLE:
+                    break;
+
+                case LOAD_COEFF:
+                    break;
+
+                default:
+                    next_state = IDLE;
+            }
+
+            master_state = next_state;
+        }
+
+
+    template <typename wb_param> \
+        void Increment<wb_param>::slaveMoore() {
+            int32_t next_state;
+
+            switch(slave_state) {
+
+                case IDLE:
+                    p_wb_slave.ACK_O = 0;
+                    break;
+
+                case WAIT_CONFIG:
+                    p_wb_slave.ACK_O = 0;
+                    break;
+
+                case ACK_AND_LOAD_COEFF:
+                    // Update the address and acknowledge the request.
+                    coeff_address_start = p_wb_slave.DAT_I.read();
+                    coeff_address_current = p_wb_slave.DAT_I.read();
+                    cout << "INCREMENT: coeff address = " << coeff_address_start << endl;
+                    // Coming from WAIT_CONFIG, we set ACK_O to 1.
+                    p_wb_slave.ACK_O = 1;
+                    break;
+
+                case LOAD_COEFF:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+    template <typename wb_param> \
+        void Increment<wb_param>::masterMoore() {
+        }
+
 }}
 
 
--- main/increment/increment.h
+++ main/increment/increment.h
@@ -11,44 +11,55 @@
 #include "interpolation.h"
 #include "buffer.h"
 
+#define N_COEFF 20
+#define COEFF_TYPE int32_t
+
 namespace soclib { namespace caba {
 
     template <typename wb_param>
+    class Increment: public sc_core::sc_module{
+        private:
+            soclib::caba::IncrementHard incHardX;
+            soclib::caba::IncrementHard incHardY;
+//            soclib::caba::Interpolation interpolation;
+//            soclib::caba::Buffer buffer;
+            sc_signal<float> dummy_float,dummy_float1;
+            sc_signal<bool> dummy_bool;
+            sc_signal<unsigned char> dummy_uchar[4];
+            sc_signal<uint32_t> dummy_uint32;
+            sc_signal<uint32_t> dummy_uint32_t[2];
+            sc_signal<unsigned char> dummy_out;
 
-        class Increment: public sc_core::sc_module{
-            private:
-                soclib::caba::IncrementHard incrementHard;
-                soclib::caba::Interpolation interpolation;
-                soclib::caba::Buffer buffer;
-                sc_signal<float> dummy_float,dummy_float1;
-                sc_signal<bool> dummy_bool;
-                sc_signal<unsigned char> dummy_uchar[4];
-                sc_signal<uint32_t> dummy_uint32;
-                sc_signal<uint32_t> dummy_uint32_t[2];
-                sc_signal<unsigned char> dummy_out;
-						protected:
-                SC_HAS_PROCESS(Increment);
-            public:
-                sc_core::sc_in<bool>              p_clk;
-                sc_core::sc_in<bool>              p_resetn;
-								//wishbone interface
-								WbMaster <wb_param>    						p_wb_master;
-								WbSlave <wb_param>								p_wb_slave;
-                // constructor
-                Increment (sc_core::sc_module_name insname);
+            int32_t     slave_state, master_state;
+            uint32_t    coeff_address_start, coeff_address_current;
 
-								void incrementTransition();
-								void incrementMoore();
-        };
+        protected:
+            SC_HAS_PROCESS(Increment);
+        public:
+            sc_core::sc_in<bool>    p_clk;
+            sc_core::sc_in<bool>    p_resetn;
+            // Wishbone interface
+            WbMaster <wb_param>     p_wb_master;
+            WbSlave <wb_param>      p_wb_slave;
+            // Constructor
+            Increment (sc_core::sc_module_name insname);
+
+            void slaveTransition();
+            void slaveMoore();
+
+            void masterTransition();
+            void masterMoore();
+
+            enum states {
+                IDLE,
+                WAIT_CONFIG,
+                ACK_AND_LOAD_COEFF,
+                LOAD_COEFF,
+                COMPUTE_MIN,
+                INCR
+            };
+    };
 
 }}
+
 #endif
-
-// Local Variables:
-// tab-width: 4
-// c-basic-offset: 4
-// c-file-offsets:((innamespace . 0)(inline-open . 0))
-// indent-tabs-mode: nil
-// End:
-
-// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=4:softtabstop=4
